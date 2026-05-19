fbcf3bab5dcc493444959268180b6d82b84b10f4
chinazagideon <chinazagideon264@gmail.com>
2026-05-19 19:41:23 +0100

    change company address

--- app/components/constants/index.ts
+++ app/components/constants/index.ts
@@ -2,7 +2,7 @@ export const constants = {
   phoneNumber: '+1 (204) 229-5871',
   companyName: 'ICANDO Movers',
   companyEmail: 'info@icandomovers.ca',
-  companyAddress: '246 Hatcher Road',
+  companyAddress: '463 Dawson road north',
   companyCity: 'Winnipeg',
   companyProvince: 'MB',
   companyPostalCode: 'R2C 3W7',
